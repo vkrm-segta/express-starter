ed7e64610c669dc789fcec51bf9a852457c8d9b4
Vikram Segta <vikramsegta43@gmail.com>
2023-07-26 13:47:26 +0530

    Create first get and post request in express 🔥

---
+++ app.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const express = require('express')
+
+const app = express();
+
+app.use(express.json())
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({ id: newId }, req.body)
+
+    tours.push(newTour)
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), () => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+})
+
+const port = 3030
+app.listen(port, () => {
+    console.log(`App is running from ${port}...`)
+})
